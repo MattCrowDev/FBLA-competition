cba670bd815346ffacf6f000ed5929d1294d6029
Matthew Crowell <overthewelldesigns@gmail.com>
2020-01-16 15:27:49 -0700

    yep i hate this

---
+++ FBLA BAA Project/Assets/Scripts/ComputerMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerMove : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float moveSpeed = 30f;
+
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        //move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        jump = false;
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Finish"))
+        {
+            Debug.Log("Hitting");
+        }
+
+    }
+
+
+
+
+
+}
---
+++ FBLA BAA Project/Assets/computerSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class computerSwitch : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("ComputerGuy"))
+        {
+            Debug.Log("Hitting");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Hitting");
+        }
+    }
+}
